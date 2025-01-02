3e5f873148734653160b25e34880558c24645e55
haruyoni <hayeoll@gmail.com>
2025-01-02 20:49:14 +0900

    junit5를 이용한 기본 테스트 실습1 - 문제점1 stub 검증 코드 메소드로 만들어서 여러 값으로 테스트

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -18,19 +18,13 @@ class PaymentServiceTest {
     @Test
     @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
     void prepare() throws IOException {
-        PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(500)));
-
-        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
-
-        // 환율 정보 가져온다
-        assertThat(payment.getExRate()).isEqualTo(valueOf(500));
-
-        // 원화 환산 금액 계산
-        assertThat(payment.getConvertedAmount()).isEqualTo(valueOf(5_000)); // _ : 자릿수 구별 문자
+        testAmount(valueOf(500), valueOf(5_000));
+        testAmount(valueOf(1_000), valueOf(10_000));
+        testAmount(valueOf(3_000), valueOf(30_000));
 
         // 원화 환산 금액의 유효시간 계산
-        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
-        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
+//        assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
+//        assertThat(payment.getValidUntil()).isBefore(LocalDateTime.now().plusMinutes(30));
 
         /*
         위 테스트 코드 문제점
@@ -38,5 +32,18 @@ class PaymentServiceTest {
         - exrate에서 받아온걸로 계산한게 맞는지 확실x -> Stub(대역)을 만들어서 검증
         - 유효시간 검증 짜침
          */
+    }
+
+    private static Payment testAmount(BigDecimal exRate, BigDecimal convertedAmount) throws IOException {
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(exRate));
+
+        Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
+
+        // 환율 정보 가져온다
+        assertThat(payment.getExRate()).isEqualTo(exRate);
+
+        // 원화 환산 금액 계산
+        assertThat(payment.getConvertedAmount()).isEqualTo(convertedAmount); // _ : 자릿수 구별 문자
+        return payment;
     }
 }
