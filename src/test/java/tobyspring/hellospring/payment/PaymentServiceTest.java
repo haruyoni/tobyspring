748ac6c5bb07c002b68b3fe767974b995d06b7cc
haruyoni <hayeoll@gmail.com>
2025-01-02 20:42:16 +0900

    junit5를 이용한 기본 테스트 실습1 - 문제점1 stub 만들어서 검증

--- src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
+++ src/test/java/tobyspring/hellospring/payment/PaymentServiceTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import static java.math.BigDecimal.valueOf;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,15 +18,15 @@ class PaymentServiceTest {
     @Test
     @DisplayName("prepare 메소드가 요구사항 3가지를 잘 충족했는지 검증")
     void prepare() throws IOException {
-        PaymentService paymentService = new PaymentService(new WebApiExRateProvider());
+        PaymentService paymentService = new PaymentService(new ExRateProviderStub(valueOf(500)));
 
         Payment payment = paymentService.prepare(1L, "USD", BigDecimal.TEN);
 
         // 환율 정보 가져온다
-        assertThat(payment.getExRate()).isNotNull();
+        assertThat(payment.getExRate()).isEqualTo(valueOf(500));
 
         // 원화 환산 금액 계산
-        assertThat(payment.getConvertedAmount()).isEqualTo(payment.getExRate().multiply(payment.getForeignCurrentAmount()));
+        assertThat(payment.getConvertedAmount()).isEqualTo(valueOf(5_000)); // _ : 자릿수 구별 문자
 
         // 원화 환산 금액의 유효시간 계산
         assertThat(payment.getValidUntil()).isAfter(LocalDateTime.now());
@@ -34,7 +35,7 @@ class PaymentServiceTest {
         /*
         위 테스트 코드 문제점
         - 외부 환경 오류
-        - exrate에서 받아온걸로 계산한게 맞는지 확실x
+        - exrate에서 받아온걸로 계산한게 맞는지 확실x -> Stub(대역)을 만들어서 검증
         - 유효시간 검증 짜침
          */
     }
