283019dedffc7b89ee5a8ec0f4104d79cf56cb65
haruyoni <hayeoll@gmail.com>
2025-01-05 20:30:29 +0900

    리팩터링 - 변동코드 메소드로 분리

--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -17,6 +17,8 @@ public class WebApiExRateProvider  implements ExRateProvider {
     @Override
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
+
+        // 이 아래로는 변경 가능성이 거의 없는 고정된 코드
         URI uri;
         try {
             uri = new URI(url);
@@ -26,22 +28,34 @@ public class WebApiExRateProvider  implements ExRateProvider {
 
         String response;
         try{
-            HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
-            try(BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
-                response = br.lines().collect(Collectors.joining());
-            }; // try()안에서 br을 생성하면 그 블럭을 빠져나가기 전에 br.close()을 자동으로 해줌
+            response = executeAPI(uri);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            ExRateData data = mapper.readValue(response, ExRateData.class);
-            return data.rates().get("KRW");
+            return parseExRate(response);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
 
 
+    }
+
+    // 변동 가능성이 있는 코드 메소드로 분리
+    private static BigDecimal parseExRate(String response) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        ExRateData data = mapper.readValue(response, ExRateData.class);
+        return data.rates().get("KRW");
+    }
+
+    private static String executeAPI(URI uri) throws IOException {
+        String response;
+        HttpURLConnection connection = (HttpURLConnection) uri.toURL().openConnection();
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
+            response = br.lines().collect(Collectors.joining());
+        }
+        ; // try()안에서 br을 생성하면 그 블럭을 빠져나가기 전에 br.close()을 자동으로 해줌
+        return response;
     }
 }
