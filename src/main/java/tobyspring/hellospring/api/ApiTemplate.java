005dbca2f1bbc5cf9dcbca6557ab40d471dce7b9
haruyoni <hayeoll@gmail.com>
2025-01-06 16:33:30 +0900

    리팩터링 - getForExRate 콜백 파라미터 디폴트값과 모든 경우의수 오버로드로 정의

--- src/main/java/tobyspring/hellospring/api/ApiTemplate.java
+++ src/main/java/tobyspring/hellospring/api/ApiTemplate.java
@@ -8,11 +8,33 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class ApiTemplate {
+
+    private final ApiExecutor apiExecutor;
+    private final ExRateExtractor exRateExtractor;
+
+    public ApiTemplate() {
+        this.apiExecutor = new HttpClientApiExecutor();
+        this.exRateExtractor = new ErApiExRateExtractor();
+    }
+
+
+
+    public BigDecimal getForExRate(String url) {
+        return this.getForExRate(url, this.apiExecutor, this.exRateExtractor);
+    }
+    public BigDecimal getForExRate(String url, ApiExecutor apiExecutor) {
+        return this.getForExRate(url, apiExecutor, this.exRateExtractor);
+    }
+    public BigDecimal getForExRate(String url, ExRateExtractor exRateExtractor) {
+        return this.getForExRate(url, this.apiExecutor, exRateExtractor);
+    }
+
+
     // 이 아래로는 변경 가능성이 거의 없는 고정된 코드 -> 템플릿
     // 템플릿 : 고정된 틀 안에 바꿀 수 있는 부분을 넣어서 사용하도록 만들어진 오브젝트
     // 템플릿 메소드 패턴 : 고정된 틀의 로직을 가진 템플릿 메소드를 슈퍼클래스에 두고, 바뀌는 부분을 서브클래스의 메소드에 두는 구조(이거 사용한 건 아님)
     // 콜백과 메소드 주입
-    public BigDecimal getExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
+    public BigDecimal getForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
         URI uri;
         try {
             uri = new URI(url);
--- src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
+++ src/main/java/tobyspring/hellospring/exrate/WebApiExRateProvider.java
@@ -1,20 +1,8 @@
 package tobyspring.hellospring.exrate;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import tobyspring.hellospring.api.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.math.BigDecimal;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.stream.Collectors;
 
 
 public class WebApiExRateProvider  implements ExRateProvider {
@@ -24,6 +12,6 @@ public class WebApiExRateProvider  implements ExRateProvider {
     public BigDecimal getExRate(String currency) {
         String url = "https://open.er-api.com/v6/latest/" + currency;
 
-        return apiTemplate.getExRate(url, new HttpClientApiExecutor(), new ErApiExRateExtractor());
+        return apiTemplate.getForExRate(url);
     }
 }
