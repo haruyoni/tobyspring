934a03b70c9df94c518668e3af2d8ab6533c2054
haruyoni <hayeoll@gmail.com>
2025-01-07 16:21:24 +0900

    JPA 실습1

---
+++ src/main/java/tobyspring/hellospring/DataClient.java
@@ -0,0 +1,33 @@
+package tobyspring.hellospring;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import tobyspring.hellospring.order.Order;
+
+import java.math.BigDecimal;
+
+public class DataClient {
+    public static void main(String[] args) {
+        BeanFactory beanFactory = new AnnotationConfigApplicationContext(DataConfig.class);
+        EntityManagerFactory emf = beanFactory.getBean(EntityManagerFactory.class);
+
+        // em
+        EntityManager em = emf.createEntityManager();
+
+        // transaction
+        em.getTransaction().begin();
+
+        // em.persist
+        Order order = new Order("100", BigDecimal.TEN);
+
+        em.persist(order);
+
+        System.out.println(order);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
